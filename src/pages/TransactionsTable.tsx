670d9a4755f9bc067183da933beb4baed9dbe12a
Harish C <harishc@globussoft.in>
2025-02-24 15:50:47 +0530

    Add No transactions state

--- src/pages/TransactionsTable.tsx
+++ src/pages/TransactionsTable.tsx
@@ -8,74 +8,74 @@ import { formatApt } from '@aptos-labs/js-pro';
 export const TransactionsTable = () => {
     const { account } = useWallet();
     const { data: activities, isLoading } = useAccountActivities({ address: account?.address });
-    console.log('activities:', activities);
 
     return (
-        <>
-
-            <div className='p-4'>
-                <h2 className='text-xl font-bold mb-4'>User Transactions</h2>
-                <div className='overflow-x-auto'>
-                    <table className='w-full border-collapse border border-gray-300'>
-                        <thead>
-                            <tr className='bg-gray-100'>
-                                <th className='border p-2'>Version</th>
-                                <th className='border p-2'>Type</th>
-                                <th className='border p-2'>Timestamp</th>
-                                <th className='border p-2'>Sender</th>
-                                <th className='border p-2'>Sent To</th>
-                                <th className='border p-2'>Amount (APT)</th>
+        <div className='p-4'>
+            <h2 className='text-xl font-bold mb-4'>User Transactions</h2>
+            <div className='overflow-x-auto'>
+                <table className='w-full border-collapse border border-gray-300'>
+                    <thead>
+                        <tr className='bg-gray-100'>
+                            <th className='border p-2'>Version</th>
+                            <th className='border p-2'>Type</th>
+                            <th className='border p-2'>Timestamp</th>
+                            <th className='border p-2'>Sender</th>
+                            <th className='border p-2'>Sent To</th>
+                            <th className='border p-2'>Amount (APT)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {isLoading ? (
+                            <tr>
+                                <td colSpan={6} className='text-center p-4'>
+                                    Loading...
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {isLoading ? (
-                                <tr>
-                                    <td colSpan={6} className='text-center p-4'>
-                                       Loading..
+                        ) : activities?.pages?.[0]?.events?.length ? (
+                            activities.pages[0].events.map((tx, index) => (
+                                <tr key={index} className='border'>
+                                    <td className='border p-2'>{tx.version.toString()}</td>
+                                    <td className='border p-2'>{tx._type}</td>
+                                    <td className='border p-2'>{new Date(tx.timestamp).toLocaleString()}</td>
+                                    <td className='border p-2'>
+                                        {tx.sender ? truncateAddress(tx.sender?.address) : 'N/A'}{' '}
+                                        {tx.sender && (
+                                            <Copy
+                                                size={12}
+                                                className='cursor-pointer inline-block ml-1'
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(tx.sender?.address);
+                                                    toast.success('Copied to clipboard!');
+                                                }}
+                                            />
+                                        )}
                                     </td>
+                                    <td className='border p-2'>
+                                        {tx.receiver ? truncateAddress(tx.receiver?.address) : 'N/A'}{' '}
+                                        {tx.receiver && (
+                                            <Copy
+                                                size={12}
+                                                className='cursor-pointer inline-block ml-1'
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(tx.receiver?.address);
+                                                    toast.success('Copied to clipboard!');
+                                                }}
+                                            />
+                                        )}
+                                    </td>
+                                    <td className='border p-2'>{tx.amount ? formatApt(tx.amount) : '0'} APT</td>
                                 </tr>
-                            ) : (
-                                activities?.pages?.[0]?.events?.map((tx, index) => (
-                                    <tr key={index} className='border'>
-                                        <td className='border p-2'>{tx.version.toString()}</td>
-                                        <td className='border p-2'>{tx._type}</td>
-                                        <td className='border p-2'>{new Date(tx.timestamp).toLocaleString()}</td>
-                                        <td className='border p-2'>
-                                            {tx.sender ? truncateAddress(tx.sender?.address) : 'N/A'}{' '}
-                                            {tx.sender ? (
-                                                <Copy
-                                                    size={12}
-                                                    onClick={() => {
-                                                        navigator.clipboard.writeText(tx.sender?.address);
-                                                        toast.success('Copied to clipboard!');
-                                                    }}
-                                                />
-                                            ) : (
-                                                ''
-                                            )}
-                                        </td>
-                                        <td className='border p-2'>
-                                            {tx.receiver ? truncateAddress(tx.receiver?.address) : 'N/A'}{' '}
-                                            {tx.receiver ? (
-                                                <Copy
-                                                    size={12}
-                                                    onClick={() => {
-                                                        navigator.clipboard.writeText(tx.receiver?.address);
-                                                        toast.success('Copied to clipboard!');
-                                                    }}
-                                                />
-                                            ) : (
-                                                ''
-                                            )}
-                                        </td>
-                                        <td className='border p-2'>{tx.amount ? formatApt(tx.amount) : '0'} APT</td>
-                                    </tr>
-                                ))
-                            )}
-                        </tbody>
-                    </table>
-                </div>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan={6} className='text-center p-4'>
+                                    No transactions found.
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
-        </>
+        </div>
     );
 };
